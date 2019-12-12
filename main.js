99d5887d803411eeacb2a467e21741d62972aae9
Benjamin Arnold <benjamin@cnl.de>
2019-12-12 08:49:46 +0100

    fix unit tests

--- main.js
+++ main.js
@@ -74,11 +74,11 @@ class MuenchenNavigator extends utils.Adapter {
 		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
 		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw ioboker: " + result);
+		//let result = await this.checkPasswordAsync("admin", "iobroker");
+		//this.log.info("check user admin pw ioboker: " + result);
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		//result = await this.checkGroupAsync("admin", "admin");
+		//this.log.info("check group user admin group admin: " + result);
 	}
 
 	/**
